1311cc92bb661b2f6eac6cba14d4863449ef3212
Vincent Wilson <vince@classyh.at>
2019-04-18 15:25:46 -0500

    Release 0.8.2

---
+++ apolloschurchapp/src/tabs/connect/getUserProfile.js
@@ -0,0 +1,25 @@
+import gql from 'graphql-tag';
+import CampusParts from 'apolloschurchapp/src/user-settings/Locations/campusFragment';
+
+export default gql`
+  query getCurrentUserProfile {
+    currentUser {
+      id
+      profile {
+        firstName
+        lastName
+        campus {
+          ...CampusParts
+        }
+        email
+        nickName
+        gender
+        birthDate
+        photo {
+          uri
+        }
+      }
+    }
+  }
+  ${CampusParts}
+`;
