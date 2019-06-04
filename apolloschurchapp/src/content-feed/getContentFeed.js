beefc4ae8c9e3bf98ad2c20fd65ee4758a380139
Vincent Wilson <vince@classyh.at>
2019-06-04 13:53:14 -0400

    Release Client 0.8.7

---
+++ apolloschurchapp/src/content-feed/getContentFeed.js
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag';
+
+import { largeCardFragment } from 'apolloschurchapp/src/ui/ContentCardConnected';
+import { contentItemFragment } from '../content-single/getContentItem';
+
+export default gql`
+  query getContentFeed($itemId: ID!, $after: String, $first: Int) {
+    node(id: $itemId) {
+      ... on ContentChannel {
+        childContentItemsConnection(after: $after, first: $first) {
+          pageInfo {
+            endCursor
+          }
+          edges {
+            node {
+              ...contentItemFragment
+              ...largeCardFragment
+            }
+          }
+        }
+      }
+    }
+  }
+  ${contentItemFragment}
+  ${largeCardFragment}
+`;
