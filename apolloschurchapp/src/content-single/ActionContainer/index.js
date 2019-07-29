a14d9f81e3f0e38d9f7164f5da370f5d967b5267
Vincent Wilson <vince@classyh.at>
2019-07-29 15:35:54 -0400

    Release Client 1.1.0-beta.2

---
+++ apolloschurchapp/src/content-single/ActionContainer/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Platform, View } from 'react-native';
+import PropTypes from 'prop-types';
+import ShareButton from 'apolloschurchapp/src/ui/ShareButton';
+
+import { SideBySideView, styled } from '@apollosproject/ui-kit';
+import LikeButton from 'apolloschurchapp/src/ui/LikeButton';
+import { MediaPlayerSpacer } from '@apollosproject/ui-media-player';
+
+const PositioningView = styled(({ theme }) => ({
+  justifyContent: 'space-around',
+  paddingVertical: theme.sizing.baseUnit / 2,
+  paddingHorizontal: theme.sizing.baseUnit,
+}))(SideBySideView);
+
+const Container = styled(({ theme }) => ({
+  backgroundColor: theme.colors.paper,
+  ...Platform.select(theme.shadows.default),
+}))(View);
+
+const ActionContainer = ({ itemId }) => (
+  <Container>
+    <MediaPlayerSpacer>
+      <PositioningView>
+        <LikeButton itemId={itemId} />
+        <ShareButton itemId={itemId} />
+      </PositioningView>
+    </MediaPlayerSpacer>
+  </Container>
+);
+
+ActionContainer.propTypes = {
+  content: PropTypes.shape({}),
+  itemId: PropTypes.string,
+};
+
+export default ActionContainer;
