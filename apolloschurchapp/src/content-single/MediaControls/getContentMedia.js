f5451e3d880bf517d76b2bb639794c128b16f086
Vincent Wilson <vince@classyh.at>
2019-10-23 17:30:47 -0400

    Release Client 1.2.0-beta.7

---
+++ apolloschurchapp/src/content-single/MediaControls/getContentMedia.js
@@ -0,0 +1,37 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  query getContentMedia($contentId: ID!) {
+    node(id: $contentId) {
+      ... on ContentItem {
+        id
+        title
+        parentChannel {
+          id
+          name
+        }
+        coverImage {
+          sources {
+            uri
+          }
+        }
+        videos {
+          sources {
+            uri
+          }
+        }
+      }
+      ... on WeekendContentItem {
+        liveStream {
+          isLive
+          media {
+            sources {
+              uri
+            }
+          }
+          webViewUrl
+        }
+      }
+    }
+  }
+`;
