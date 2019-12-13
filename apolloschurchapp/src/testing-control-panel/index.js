e42ff03149426f7c322f1bf52348cf5a526119c2
Vincent Wilson <vince@classyh.at>
2019-12-13 10:37:03 -0500

    Release Client 1.2.5

---
+++ apolloschurchapp/src/testing-control-panel/index.js
@@ -0,0 +1,52 @@
+import React, { PureComponent } from 'react';
+import { TableView } from '@apollosproject/ui-kit';
+import { UserWebBrowserConsumer } from 'apolloschurchapp/src/user-web-browser';
+import { WebBrowserConsumer } from 'apolloschurchapp/src/ui/WebBrowser';
+import TouchableCell from './TouchableCell';
+
+export default class TestingControlPanel extends PureComponent {
+  static navigationOptions = () => ({
+    title: 'Testing Control Panel',
+  });
+
+  render() {
+    return (
+      <TableView>
+        <UserWebBrowserConsumer>
+          {(openUserWebView) => (
+            <TouchableCell
+              handlePress={() =>
+                openUserWebView({
+                  url:
+                    'https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending',
+                })
+              }
+              iconName="share"
+              cellText={`Open Web Browser With User Cookie`}
+            />
+          )}
+        </UserWebBrowserConsumer>
+        <WebBrowserConsumer>
+          {(openUrl) => (
+            <TouchableCell
+              handlePress={() =>
+                openUrl(
+                  'https://apollosrock.newspring.cc',
+                  {},
+                  { useRockToken: true }
+                )
+              }
+              iconName="share"
+              cellText={`Open InAppBrowser With Rock Token`}
+            />
+          )}
+        </WebBrowserConsumer>
+        <TouchableCell
+          handlePress={() => this.props.navigation.navigate('Onboarding')}
+          iconName="Avatar"
+          cellText={`Launch Onboarding`}
+        />
+      </TableView>
+    );
+  }
+}
